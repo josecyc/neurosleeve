442e29077abf5c126e049c05347939431008c030
DanielCordovaV <daniel_cordova_98@outlook.com>
2019-04-12 17:51:08 -0700

    Setting repo structure

---
+++ data/osc_data/build_csvs.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import pandas as pd
+
+ROOT = '.'
+LABELS = ['right', 'go', 'left', 'stop', 'neutral', 'omega', 'alpha']
+LABEL_DIRS = {label: os.path.join(ROOT, label) for label in LABELS}
+LABEL_FILES = {label: [os.path.join(ROOT, label, file) for file in os.listdir(label)] for label in LABELS}
+SAMPLES = ['ch{}_sample_{}'.format(channel, sample)for channel in range(1,5)  for sample in range(1,51) ]
+LABEL_DFS = {key: pd.DataFrame(np.array([pd.read_csv(file).transpose().values.flatten() for file in LABEL_FILES[key]]), columns=SAMPLES) for key in LABELS}
+for key in LABELS:
+    LABEL_DFS[key] = LABEL_DFS[key].reindex(np.random.permutation(LABEL_DFS[key].index))
+    LABEL_DFS[key]['label'] = np.repeat(key, LABEL_DFS[key].shape[0])
+train = pd.concat([LABEL_DFS[label].iloc[:int(LABEL_DFS[label].shape[0]*.8)] for label in LABELS], sort=False, ignore_index=True)
+train = train.reindex(np.random.permutation(train.index))
+validation = pd.concat([LABEL_DFS[label].iloc[int(LABEL_DFS[label].shape[0]*.8):int(LABEL_DFS[label].shape[0]*.9)] for label in LABELS], sort=False, ignore_index=True)
+validation = validation.reindex(np.random.permutation(validation.index))
+test = pd.concat([LABEL_DFS[label].iloc[int(LABEL_DFS[label].shape[0]*.9):] for label in LABELS], sort=False, ignore_index=True)
+test = test.reindex(np.random.permutation(test.index))
+train.to_csv('train.csv', index=False)
+validation.to_csv('validation.csv', index=False)
+test.to_csv('test.csv', index=False)
